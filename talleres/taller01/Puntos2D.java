77f45ac75b01209a0d2ed080713b2ac2e7bae632
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-07-29 07:59:59 -0500

    Add files via upload

---
+++ talleres/taller01/Puntos2D.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Puntos2D here.
+ * 
+ * @author (Santiago Ochoa Castaño y Miguel Angel Zapata Jimenez) 
+ * @version (a version number or a date)
+ */
+public class Puntos2D
+{
+    private int x,y;
+
+    public Puntos2D(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+}
