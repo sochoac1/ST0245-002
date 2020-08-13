1c8271da9f0b1b81eb2f724b67ccacc07fa6bbee
Miguel Zapata <68884489+miguelzapata042@users.noreply.github.com>
2020-08-12 22:26:31 -0500

    Add files via upload

---
+++ talleres/taller03/Ejercicio3.java
@@ -0,0 +1,21 @@
+public class Ejercicio3
+{
+   /**
+    * @author (Miguel Angel Zapata Jimenez & Santiago Ochoa Castaño)
+    * @version (1)
+    */
+   
+   public static void permutacion ( String str ) {  
+        permutationAux ("",str);
+    }
+
+   private static void  permutationAux (String pref ,String  str ) {
+        if(str.length() == 0){
+            System.out.println(pref);
+        }else{
+           for(int i = 0; i < str.length(); i++){
+               permutationAux(pref + str.charAt(i), str.substring(0,i) + str.substring(i+1, str.length()));
+            }
+        }
+    }
+}
