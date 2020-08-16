6b62d70612300d05609ea32c7cbac93acbbd5f18
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-08-16 13:18:40 -0500

    Add files via upload

---
+++ proyecto/codigo/prueba.py
@@ -0,0 +1,19 @@
+import codecs
+
+results = []
+
+with codecs.open('datos0.csv', encoding = 'utf-8-sig') as f:
+
+ 	for row in f:
+
+ 		results.append(row)
+
+cont=0
+results2=[]
+
+for row in results:	
+	row1 = row.strip()	
+	results2.append(row1.split(";"))
+
+
+
