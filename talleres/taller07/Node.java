c0303b17aa3aaee39bebf40ed8d80ccbfd64a000
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-09-09 11:44:49 -0500

    Add files via upload

---
+++ talleres/taller07/Node.java
@@ -0,0 +1,10 @@
+// Un nodo para una lista simplemente enlazada
+public class Node {
+    public Bees data;
+    public Node next;
+    public Node(Bees data)
+    {
+        next = null;
+        this.data = data;
+    }
+}
