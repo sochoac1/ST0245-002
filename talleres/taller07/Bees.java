6a336af4f58d8dcc82cf8b642c256c13a01e9ca8
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-09-09 11:43:41 -0500

    Add files via upload

---
+++ talleres/taller07/Bees.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class bees here.
+ * 
+ * @author (Santiago Ochoa Castaño & Miguel Angel Zapata Jimenez) 
+ * @version (a version number or a date)
+ */
+public class Bees
+{
+    double x;
+    double y;
+    double z;
+
+    public Bees(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
