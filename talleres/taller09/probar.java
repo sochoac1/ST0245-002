d0c26bd12e1bf8bd6d5ec227650b55ea81048342
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-10-07 18:22:45 -0500

    Add files via upload

---
+++ talleres/taller09/probar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class probar
+{
+    public static void main(String[] args){
+        HashMap<String, String> elMapa = new HashMap<String, String>();
+        THashmap.agregar(elMapa, "sony", "japon");
+        THashmap.agregar(elMapa, "Nokia", "Finlandia");
+        THashmap.agregar(elMapa, "La locura", "Colombia");
+        THashmap.agregar(elMapa, "Google", "Estados Unidos");        
+    }
+}
+
