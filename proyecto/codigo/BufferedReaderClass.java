60cdf6e323b5e4d20873c6d6c57043e60021c09f
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2020-11-17 15:57:38 -0500

    Add files via upload

--- proyecto/codigo/BufferedReaderClass.java
+++ proyecto/codigo/BufferedReaderClass.java
@@ -1,5 +1,5 @@
 import java.io.*;
-
+import java.util.*;
 /**
  * Write a description of class BufferedReaderClass here.
  * 
@@ -9,8 +9,9 @@ import java.io.*;
 public class BufferedReaderClass
 {
     public  String matriz[][];
-    public void readData() throws Exception{
-        BufferedReader br = new BufferedReader(new FileReader("datotes.csv"));
+    public static List<Integer> miLista = new ArrayList<Integer>(List.of(0, 1, 2, 3, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,27, 30, 31, 32, 33, 34, 37, 38, 39, 40, 41, 43, 44, 46, 47, 48, 49, 50, 51, 55,56, 57, 58, 61, 62, 63, 64, 75, 76));
+    public BufferedReaderClass(String s) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(s));
         String line = br.readLine();
         int count = 0;
         while((line = br.readLine())!= null){
@@ -19,18 +20,23 @@ public class BufferedReaderClass
         System.out.println(count);
         br.close();
         /////////////////////////////////////////////////////////////
-        matriz = new String[count][7];
-        BufferedReader br2 = new BufferedReader(new FileReader("datotes.csv"));
-        int j = 0; 
+        matriz = new String[count][30];
+        BufferedReader br2 = new BufferedReader(new FileReader(s));
         line = br2.readLine();
-        while( (line = br2.readLine()) != null){
-            String[] lineArray = line.split(",");
-            
-            for(int i = 0; i < 7; i++){
-                matriz[j][i] = lineArray[i];
+        int z= 0;
+        int v =0;
+        while((line = br2.readLine()) != null){
+            String[] splitted = line.split(";");
+            for(int i =0; i< splitted.length; i++) {
+                if(miLista.contains(i)){
+                    continue;                
+                }else{
+                    matriz[z][v]= splitted[i];
+                    v++;
+                } 
             }
-            j++;
+            v=0;
+            z++;
         }
-        br2.close();
     }
 }
--- proyecto/codigo/main.java
+++ proyecto/codigo/main.java
@@ -10,13 +10,21 @@ import javafx.util.Pair;
 public class main
 {
     public static void main(String[] args) throws Exception{
-        String fruta[][] = { { "Green", "3", "Apple"},
-                {"Yellow", "3", "Apple"},
-                {"Red",    "1", "Grape"},
-                {"Red",    "1", "Grape"},
-                {"Yellow", "3", "Lemon"}};
-                
-        BufferedReaderClass m = new BufferedReaderClass();
+        BufferedReaderClass br = new BufferedReaderClass("prueba135000.csv");
+        BuildTree t = new BuildTree();
+        long start = System.currentTimeMillis();
+        Node y = t.decisionTree(br.matriz);
+        long finalT = System.currentTimeMillis();
+        System.out.println("It's takes " + (finalT -start));
+        
+        BufferedReaderClass br2 = new BufferedReaderClass("test135000.csv");
+        double[] in = Test.information(br2.matriz, y);
+        System.out.println("Precision: " + in[0]);
+        System.out.println("Sensibilidad: " + in[1]);
+        System.out.println("Exactitud: " + in[2]);
+        //Node root = new Node(l1.Matriz);
+        /*        
+        //BufferedReaderClass m = new BufferedReaderClass();
         m.readData();
         TreeSet<String> peye = arbol.valoresSinRep(m.matriz, 0);
         int[] pruebaEtiquetas = arbol.contarEtiquetasDeUnaMatriz(m.matriz);
